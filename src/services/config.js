32ba057934afeb25c620c3f7bb7594ffd7c97be1
suryakumar120 <gsuryakumar@iitkgp.ac.in>
2023-05-07 19:16:28 +0530

    building
    building first step

---
+++ src/services/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZBgMtxsIbDFO2_IrdxGeQ4fMVV4fftb8",
+  authDomain: "ecommerce-1b6bf.firebaseapp.com",
+  databaseURL: "https://ecommerce-1b6bf-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ecommerce-1b6bf",
+  storageBucket: "ecommerce-1b6bf.appspot.com",
+  messagingSenderId: "809944175581",
+  appId: "1:809944175581:web:f1910ce05a5a8cb8825c34",
+  measurementId: "G-PFBWFYK04P"
+};
+export default firebaseConfig
